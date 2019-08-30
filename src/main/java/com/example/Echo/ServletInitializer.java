b8f51ad721f56459b2207d92a5068f5fb369d6e7
aznm12 <aznm12@archivsoft.com>
2019-08-30 13:26:20 +0900

    first

---
+++ src/main/java/com/example/Echo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.Echo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EchoApplication.class);
+	}
+
+}
